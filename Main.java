72ab88b9f68b35765d048e11a8297eb8a6246cac
mansi <mansi4410@gmai.com>
2025-06-02 12:39:18 +0530

    added new file

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
         System.out.println("this is java program");
         System.out.println("output from java program");
         System.out.println("Hello world");
+        System.out.println("added new file");
     }
 }
